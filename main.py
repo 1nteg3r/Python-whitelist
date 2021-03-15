01e67c14d71a3bff67ed02c4a3acee39d3db5c4d
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2021-03-15 09:34:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 whitelisted_name = ["name1", "name2","name3"]           #enter the whitelisted items(names,items,codes etc...)
 name = input("Enter your name ")                        #takes user input as assigns it as variable "name"
 if name in whitelisted_name:                            #checks if the input you entered is whitelisted
-    print("You're name is whitelisted: " + name) 
+    print("Your name is whitelisted: " + name) 
 else:
-    print("You're name is not whitelisted:  " + name)
+    print("Your name is not whitelisted:  " + name)
